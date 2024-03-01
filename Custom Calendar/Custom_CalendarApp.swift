558d77d197abaaf217663cbc432fe2bcf4331f62
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-02-29 22:03:45 -0500

    Initial Commit

---
+++ Custom Calendar/Custom_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CalendarApp.swift
+//  Custom Calendar
+//
+//  Created by Leonard McCook-Carr on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalendarView()
+        }
+    }
+}
